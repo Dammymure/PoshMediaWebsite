af37fa1f64772da35f827c5e7b0ab60247cd47f9
Dammymure <dammymure1@gmail.com>
2024-01-11 20:42:24 +0100

    i completed the landing page of the portfolio

---
+++ index.js
@@ -0,0 +1,28 @@
+// const demobtn = document.getElementById("demo-btn");
+// const popup = document.querySelector(".popup-1"); // Use querySelector instead of getElementsByClassName
+// const closepop = document.getElementById("close-btn");
+
+// demobtn.addEventListener("click", () => {
+//  popup.classList.remove('hidden');
+//  console.log("open");
+// });
+
+// closepop.addEventListener("click", () => {
+//  popup.classList.add('hidden');
+//  console.log("close");
+// });
+
+
+// const demobtn2 = document.getElementById("demo-btn2");
+// const popup2 = document.querySelector(".popup-2"); // Use querySelector instead of getElementsByClassName
+// const closepop2 = document.getElementById("close-btn2");
+
+// demobtn2.addEventListener("click", () => {
+//  popup2.classList.remove('hidden');
+//  console.log("open");
+// });
+
+// closepop2.addEventListener("click", () => {
+//  popup2.classList.add('hidden');
+//  console.log("close");
+// });
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {},
+    screens:{
+      xs:'380px',
+      sm:'540px',
+      md:'720px',
+      lg:'920px',
+      xl:'1040px'
+    }
+  },
+  plugins: [],
+}
+
